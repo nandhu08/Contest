c412b8c9f41d37bf5e0ab8ac2505676d9a40fd74
nandhu08 <knandacse.kce@gmail.com>
2017-12-27 13:17:01 +0530

    heroku

--- src/App.js
+++ src/App.js
@@ -8,10 +8,16 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to my page</h1>
         </header>
+        <h1>Nanda kumar</h1>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          
+          In ea commodo laboris eu aute magna fugiat quis. Amet commodo do ex eiusmod occaecat cillum irure et pariatur. Esse quis enim reprehenderit minim incididunt est duis.
+
+Aliquip elit amet exercitation Lorem esse commodo proident occaecat. Est duis velit eu nostrud cupidatat ullamco sunt ea ea excepteur pariatur dolor eu quis. Voluptate culpa consequat ullamco culpa consequat reprehenderit aliquip est ipsum occaecat ea dolor. In et sint cillum officia aute occaecat Lorem mollit deserunt officia.
+
+Eiusmod magna qui exercitation ex. Culpa duis nostrud incididunt esse mollit aliquip. Eu nisi ea Lorem est aute commodo enim in culpa eiusmod deserunt commodo nulla.
         </p>
       </div>
     );
